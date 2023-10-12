1d1bed7a558137122712623b64989638c06f317a
Bernard Towindo <bernardtowindo38@gmail.com>
2023-10-12 22:03:44 +0200

    New Activities Added

---
+++ app/src/main/java/com/example/hutchhub/Screen.java
@@ -0,0 +1,45 @@
+package com.example.hutchhub;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+
+public class Screen extends AppCompatActivity {
+
+    private TextView getStarted;
+    FirebaseUser LoggediNuser;
+    private FirebaseAuth mAuth;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_screen);
+
+        getStarted = findViewById(R.id.btn_get_started);
+        mAuth = FirebaseAuth.getInstance();
+
+        LoggediNuser = mAuth.getCurrentUser();
+
+
+        getStarted.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                startActivity(new Intent(Screen.this, Login.class ));
+
+                if(LoggediNuser!= null){
+                    LoggediNuser.getUid();
+
+                }else {
+                    startActivity(new Intent(Screen.this,Login.class));
+                    finish();
+                }
+            }
+        });
+
+    }
+}
