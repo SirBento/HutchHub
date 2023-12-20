90b9b2e3489691ae3a57b716faf9c500015f3cdb
Bernard Towindo <bernardtowindo38@gmail.com>
2023-12-20 19:16:52 +0200

    Fix: Buy-Sell-Chat

--- app/src/main/java/com/example/hutchhub/BreedingCare.java
+++ app/src/main/java/com/example/hutchhub/BreedingCare.java
@@ -1,10 +1,6 @@
 package com.example.hutchhub;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.SearchView;
-import androidx.recyclerview.widget.RecyclerView;
-
 import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
@@ -13,18 +9,10 @@ import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Toast;
-
-import com.example.hutchhub.Adapters.BreadingAndCareAdapater;
-import com.example.hutchhub.Models.BreedingAndCare;
-import com.example.hutchhub.Models.RabbitForSale;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.HashMap;
 import java.util.Locale;
